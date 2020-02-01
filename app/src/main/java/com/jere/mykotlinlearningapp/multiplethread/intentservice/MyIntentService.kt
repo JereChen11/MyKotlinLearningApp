d1c26064d2dc6ebe510385ef38575efd37ed3537
JereChen <jerechen11@gmail.com>
2020-02-01 15:02:41 +0800

    add IntentService update UI

--- app/src/main/java/com/jere/mykotlinlearningapp/multiplethread/intentservice/MyIntentService.kt
+++ app/src/main/java/com/jere/mykotlinlearningapp/multiplethread/intentservice/MyIntentService.kt
@@ -2,30 +2,45 @@ package com.jere.mykotlinlearningapp.multiplethread.intentservice
 
 import android.app.IntentService
 import android.content.Intent
+import android.os.Bundle
+import android.os.Message
+import android.os.Messenger
 import android.util.Log
 import android.widget.Toast
 
 class MyIntentService : IntentService("MyIntentService") {
     companion object {
         private val TAG: String = "MyIntentService"
-        public val DOWNLOAD_ACTION = "DOWNLOAD_ACTION"
-        public val READ_ACTION = "READ_ACTION"
-        public val TEST_AUTHOR = "TEST_AUTHOR"
+        val DOWNLOAD_ACTION_KEY = "DOWNLOAD_ACTION"
+        val READ_ACTION_KEY = "READ_ACTION"
+        val TEST_AUTHOR_KEY = "TEST_AUTHOR"
+        val TEST_MESSENGER_KEY = "TEST_MESSENGER"
+        val DOWNLOAD_PROGRESS_BAR_VALUE_KEY = "DOWNLOAD_PROGRESS_BAR_VALUE"
     }
 
 
     override fun onHandleIntent(intent: Intent?) {
         val action: String? = intent?.action
-        val author: String? = intent?.extras?.getString(TEST_AUTHOR)
+        val author: String? = intent?.extras?.getString(TEST_AUTHOR_KEY)
 
         when (intent?.action) {
-            DOWNLOAD_ACTION -> {
-                for (i in 0..10) {
+            DOWNLOAD_ACTION_KEY -> {
+                for (i in 0..6) {
                     Thread.sleep(1000)
                     Log.e(TAG, "$author $action $i")
+                    //获取从 Activity 中传入的 Messenger 对象
+                    val messenger: Messenger = intent.extras?.get(TEST_MESSENGER_KEY) as Messenger
+                    //新建一个 Message 对象
+                    val msg: Message = Message()
+                    //为 Message 对象设置 下载进度 参数
+                    val bundle: Bundle = Bundle()
+                    bundle.putInt(DOWNLOAD_PROGRESS_BAR_VALUE_KEY, i)
+                    msg.data = bundle
+                    //Messenger 回复消息给 Activity
+                    messenger.send(msg)
                 }
             }
-            READ_ACTION -> {
+            READ_ACTION_KEY -> {
                 for (j in 0..10) {
                     Thread.sleep(2000)
                     Log.e(TAG, "$author $action $j")
@@ -37,13 +52,19 @@ class MyIntentService : IntentService("MyIntentService") {
     override fun onCreate() {
         super.onCreate()
         Log.e(TAG, "onCreate")
-        Toast.makeText(this, "conCreate", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.e(TAG, "onStartCommand")
         Toast.makeText(this, "onStartCommand", Toast.LENGTH_SHORT).show()
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onStart(intent: Intent?, startId: Int) {
+        super.onStart(intent, startId)
+        Log.e(TAG, "onStart")
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/jere/mykotlinlearningapp/multiplethread/intentservice/TestIntentServiceActivity.kt
+++ app/src/main/java/com/jere/mykotlinlearningapp/multiplethread/intentservice/TestIntentServiceActivity.kt
@@ -2,8 +2,13 @@ package com.jere.mykotlinlearningapp.multiplethread.intentservice
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.os.Messenger
 import androidx.appcompat.app.AppCompatActivity
 import com.jere.mykotlinlearningapp.R
+import kotlinx.android.synthetic.main.activity_test_intent_service.*
+import java.lang.ref.WeakReference
 
 class TestIntentServiceActivity : AppCompatActivity() {
 
@@ -13,14 +18,39 @@ class TestIntentServiceActivity : AppCompatActivity() {
 
         val intent = Intent()
         intent.setClass(this, MyIntentService::class.java)
-        intent.action = MyIntentService.DOWNLOAD_ACTION
-        intent.putExtra(MyIntentService.TEST_AUTHOR, "Jere")
+        intent.action = MyIntentService.DOWNLOAD_ACTION_KEY
+        intent.putExtra(MyIntentService.TEST_AUTHOR_KEY, "Jere")
+        //将 Messenger 传递给 IntentService, 使其传递消息回来，实现客户端与服务端之间进行沟通
+        intent.putExtra(MyIntentService.TEST_MESSENGER_KEY, Messenger(MessengerHandler(this)))
         startService(intent)
 
-        val jamesIntent = Intent()
-        jamesIntent.setClass(this, MyIntentService::class.java)
-        jamesIntent.action = MyIntentService.READ_ACTION
-        jamesIntent.putExtra(MyIntentService.TEST_AUTHOR, "James")
-        startService(jamesIntent)
+//        val jamesIntent = Intent()
+//        jamesIntent.setClass(this, MyIntentService::class.java)
+//        jamesIntent.action = MyIntentService.READ_ACTION_KEY
+//        jamesIntent.putExtra(MyIntentService.TEST_AUTHOR_KEY, "James")
+//        startService(jamesIntent)
+    }
+
+    /**
+     * 此 Handler 用于创建 Messenger 对象，接收 IntentService 回复回来的消息
+     *
+     * 静态内部类，防止内存泄漏
+     */
+    class MessengerHandler(activity: TestIntentServiceActivity) : Handler() {
+        var weakReference: WeakReference<TestIntentServiceActivity> = WeakReference(activity)
+
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            //msg 为 IntentService 回复的消息，包含 Bundle 等信息。
+            val bundle: Bundle = msg.data
+            //获取 IntentService 传递过来的 下载进度 参数
+            val downloadProgressBarValue: Int =
+                bundle.get(MyIntentService.DOWNLOAD_PROGRESS_BAR_VALUE_KEY) as Int
+            val activity: TestIntentServiceActivity? = weakReference.get()
+            //将下载进度设置成 ProgressBar 的进度，显示出来。
+            if (activity != null && !activity.isFinishing) {
+                activity.intentServiceDownloadProgressBar.progress = downloadProgressBarValue
+            }
+        }
     }
 }
